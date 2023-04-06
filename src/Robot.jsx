4127ac3da3e283b9fd3b5476da76c96159ba4767
Chris Andrews <87739144+ChrisAndrewsDev@users.noreply.github.com>
2023-04-06 12:54:22 +1000

    .

--- src/Robot.jsx
+++ src/Robot.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Robot = () => {
+    // State
   const [x, setX] = useState(null);
   const [y, setY] = useState(null);
   const [sucess, setSuccess] = useState('')
@@ -8,11 +9,11 @@ const Robot = () => {
   const [error, setError] = useState(null);
   const [robotPlaced, setRobotPlaced] = useState(false);
   const [output, setOutput] = useState(null);
-
+    // Setting bounds of grid
   const isValidPlacement = (x, y) => {
     return x >= 0 && x <= 5 && y >= 0 && y <= 5;
   };
-
+    // Place on grid and facing
   const place = (x, y, facing) => {
     if (isValidPlacement(x, y)) {
       setX(x);
@@ -23,10 +24,9 @@ const Robot = () => {
       setSuccess(true);
     } else {
       setError('Invalid placement');
-      console.log('Invalid Placement!')
     }
   };
-
+    // Initial placement before allowing moves
   const move = () => {
     if (!robotPlaced) {
       setError('Robot must be placed before moving!');
@@ -52,18 +52,19 @@ const Robot = () => {
         default:
         break;
     }
-
+    // Checking to see if robot exceeds 5x5 grid - SUCCESS
     if (isValidPlacement(newX, newY)) {
       setX(newX);
       setY(newY);
       setError(null);
       setSuccess('Valid Move!')
     } else {
+        // Checking to see if robot exceeds 5x5 grid - FAIL
       setError('Invalid move, Robot will go over the edge!');
       setSuccess(null)
     }
   };
-
+    // Turn left
   const left = () => {
     if (!robotPlaced) {
       setError('Robot must be placed before moving!');
@@ -91,7 +92,7 @@ const Robot = () => {
 
     setFacing(newFacing);
   };
-
+    // Turn right
   const right = () => {
     if (!robotPlaced) {
       setError('Robot must be placed before moving!');
@@ -119,13 +120,13 @@ const Robot = () => {
 
     setFacing(newFacing);
   };
-
+//   Outputs the current location and facing
   const report = () => {
     if (!robotPlaced) {
       setError('Robot not placed');
       return;
     }
-    console.log(`${x}, ${y}, ${facing}`);
+    // Outputs facing on screen
     setOutput(`${x}, ${y}, ${facing}`)
   };
 
@@ -135,12 +136,15 @@ const Robot = () => {
       <p>Enter commands to control the robot:</p>
       {error && <div className="error">{error}</div>}
       <p>{sucess}</p>
+      {/* Live position tracker */}
       <p>{x},{y},{facing}</p>
+      {/* Inital placement at 0, 0, NORTH */}
       <button onClick={() => place(0, 0, 'NORTH')}>Place 0, 0, NORTH</button>
       <button onClick={() => move()}>Move</button>
       <button onClick={() => left()}>Left</button>
       <button onClick={() => right()}>Right</button>
       <button onClick={() => report()}>Report</button>
+      {/* Outputs position, facing when "report" click handler is triggered */}
       <p>{output}</p>
     </div>
   );
